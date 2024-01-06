41eaa30236564fc2ff169dc930451ed38c86d278
Aarif S <aarifcad01@brightskills.com>
2024-01-06 18:01:54 +0530

    script added

--- js/scriptjs.js
+++ js/scriptjs.js
@@ -1,1 +1,28 @@
-hi arav
+console.log ("welcome to javascript")
+console.log(10);
+console.log(10.10);
+console.log([10,20,30,40]);
+console.table([10,20,30,40]);
+console.log(true);
+var a=14;
+let b=7;
+const c=10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+a=6;
+console.log(a);
+b=22;
+console.log(b);
+console.log(c);
+a=6.5
+console.log(a);
+b=8.5;
+console.log(b);
+
+a="Aarif";
+console.log(a);
+b="dinesh";
+console.log(b);
